60da06acb3815ef10d5c991067bf622c3584ac4f
Justin Johnson <jjohnson103@student.butte.edu>
2017-01-26 23:17:26 +0000

    lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,14 @@
+/* start the program.
+there are two numbers the first number is 1 and the second number is 50.
+The computer must choose a number in-between 1 and 50. 
+Then the number will be stored in memory by the computer.
+Then the computer will output then type the directions of the game.
+“The user will lose 1 point for each attempt it takes. There are 10 attempts. 
+There are 10 possible points in total. 
+If the user has not guessed the right number by the end of the 10th guess the program will inform the user, they have lost.
+Then the computer will prompt the user to try to guess the correct number from 1 to 50.
+If the user chooses a number a number lower than the number chosen by the computer, the user will be told to choose a higher number.
+If the user chooses a number higher than the number chosen by the computer, it will be told to choose a lower number.
+Once the user has chosen the correct number stored by the computer it will prompt the user that it has guessed correctly!”
+
+*\
