cc6186ee5cb32c7d82c514d18f78e6a899f4935c
Justin Johnson <jjohnson103@student.butte.edu>
2017-01-31 22:54:39 +0000

    Lab1.2 Commit

---
+++ lab12.cpp
@@ -0,0 +1,22 @@
+// created by: Justin Johnson
+// created on: 1/31/17
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+cout << "           ___   "    << endl;
+cout << "         ( '_' ) "    << endl;
+cout << "       __       __ "  << endl;
+cout << "      (     .     ) " << endl;
+cout << "       __   .   __"   << endl;
+cout << "         /  . \\  "    << endl;
+cout << "        /      \\ "    << endl;
+cout << "       (  /  \\  ) "   << endl;
+
+}
+
+
+
+
