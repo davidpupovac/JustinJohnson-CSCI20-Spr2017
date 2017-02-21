df65e1c7717b52bb4944fc949beeed0d773ac472
Justin Johnson <jjohnson103@student.butte.edu>
2017-02-21 23:10:22 +0000

    Lab2.1 submision

---
+++ lab21/lab21.cpp
@@ -0,0 +1,48 @@
+// created by Justin Johnson
+// created on 2/21/17
+// 
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stdlib.h> 
+using namespace std;
+
+int main (){
+    
+    double tempF = 0.0;   // fahrenheit 
+    double windSpeed = 0.0;
+    double oldstyle = 0.0;
+    double newStyle = 0.0;
+    double diffOfStyles = 0.0;
+    double sqrtWind = 0.0;
+    double powWind = 0.0;
+    
+    
+    cout << "Enter the temperature in fahrenheit."  << endl;
+    cin >> tempF;
+    cout <<  "Enter wind speed." << endl;
+    cin >> windSpeed; 
+    sqrtWind = sqrt(windSpeed);
+    
+    oldstyle = 0.081 * (3.71 * sqrtWind + 5.81 - 0.25 * windSpeed) * (tempF - 91.4) + 91.4; 
+   cout << "The old style formula for wind chill produced " << endl;
+    cout << oldstyle << endl;
+    
+    cout << " and the new style produced. " << endl;
+    powWind = pow(windSpeed, 0.16);
+    newStyle = 35.74 + 0.6215 * tempF - 35.75 *  powWind + 0.4275 * tempF * powWind;
+    cout << newStyle << endl;
+    
+    diffOfStyles = abs(oldstyle - newStyle);
+    
+    cout << "The differnce between the styles is " << endl; 
+    cout << diffOfStyles << endl;
+    cout << " " << endl;
+    
+    cout << "windSpeed:    Old formula:     New formula:      differnce:" << endl;
+    cout << "   " << windSpeed <<"          " << oldstyle << "           " << newStyle << "            " << diffOfStyles << endl;   
+     
+    
+    return 0;
+}
