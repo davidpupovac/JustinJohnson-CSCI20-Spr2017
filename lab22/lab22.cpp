382c5151741da2f6e97ffadc302c72983424870f
Justin Johnson <jjohnson103@student.butte.edu>
2017-02-23 22:04:26 +0000

    Lab2.2 submission

---
+++ lab22/lab22.cpp
@@ -0,0 +1,23 @@
+// created by Justin Johnson
+// created on 2/21/17
+// creating random numbers with srand(); within a function
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void randomNumbers() {  // generates random nuumbers
+    
+    rand();
+    cout << "Your random number is ";
+    cout << 1 + rand() % 100;
+}
+
+
+int main(){
+
+randomNumbers();
+
+    return 0;
+};
