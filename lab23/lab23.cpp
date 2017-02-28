5b72c5c73aa9c44d6da8f88cc391d7ed90b3b374
Justin Johnson <jjohnson103@student.butte.edu>
2017-02-28 23:04:47 +0000

    Lab2.3 submission

--- lab23/lab23.cpp
+++ lab23/lab23.cpp
@@ -1,23 +1,23 @@
 // created by Justin Johnson
 // created on 2/27/17
-
+// will be swaping inputed numbers and generating a randome number from those numbers.
 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
-void randomNumbers(int &upperLimit,int &lowerLimit, int &randNum) { 
+void randomNumbers(int &upperLimit,int &lowerLimit, int &randNum) {   // will genrate random numbers
      
        srand(time(0));
-      randNum = lowerLimit + rand() % upperLimit; 
+      randNum = lowerLimit + rand() % upperLimit;   // gernrate random number between lower and upper limits
       cout << " your random number between " << lowerLimit <<  " and " << upperLimit << " is after being swaped is " << randNum;
-     }
-void numSwap(int &lowerLimit, int &upperLimit){
+     }  
+void numSwap(int &lowerLimit, int &upperLimit){  // will swap the upper and lower limits 
 
     lowerLimit = lowerLimit + upperLimit;
     upperLimit = lowerLimit - upperLimit;
-    lowerLimit = lowerLimit - upperLimit;
+    lowerLimit = lowerLimit - upperLimit;  // how the numbers are swaped
     cout << " The lower limit after being swaped is " << lowerLimit << ". The upper Limit after being swaped is " << upperLimit << endl;
      }
      
@@ -25,7 +25,7 @@ int main(){
     
     int upperLimit , lowerLimit;
     
-    int randNum = 0;
+    int randNum = 0;   
     
     cout << " Pick a number for you lower limit" << endl;
     cin >> lowerLimit;
@@ -33,12 +33,15 @@ int main(){
     cin >> upperLimit;
     cout << " The limts you choose are " << lowerLimit << " and " << upperLimit << endl;
     
-    numSwap(lowerLimit,upperLimit);
+    numSwap(lowerLimit,upperLimit);   // run function for swaping limits
     
-    randomNumbers(lowerLimit,upperLimit,randNum);
+    randomNumbers(lowerLimit,upperLimit,randNum);  // function for generating a random numnber
      
      return 0;
 }
+
+
+
 
 
 
