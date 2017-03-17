75eb05b552e401515b9707a50f6b20ad67fcd26f
Justin Johnson <jjohnson103@student.butte.edu>
2017-03-17 03:54:23 +0000

    Lab2.1 submission

--- lab21/lab21.cpp
+++ lab21/lab21.cpp
@@ -11,36 +11,36 @@ using namespace std;
 int main (){
     
     double tempF = 0.0;   // fahrenheit 
-    double windSpeed = 0.0;
-    double oldstyle = 0.0;
-    double newStyle = 0.0;
-    double diffOfStyles = 0.0;
-    double sqrtWind = 0.0;
-    double powWind = 0.0;
+    double windSpeed = 0.0; // will be velocity
+    double oldstyle = 0.0;  //is going to  old windchill formula
+    double newStyle = 0.0; // is going to be new windchill
+    double diffOfStyles = 0.0; // diffence between the formulas
+    double sqrtWind = 0.0;   // will be square root of velocity
+    double powWind = 0.0;  // will be windspeed to a power
     
     
     cout << "Enter the temperature in fahrenheit."  << endl;
     cin >> tempF;
     cout <<  "Enter wind speed." << endl;
     cin >> windSpeed; 
-    sqrtWind = sqrt(windSpeed);
+    sqrtWind = sqrt(windSpeed);  // square root of velocity
     
-    oldstyle = 0.081 * (3.71 * sqrtWind + 5.81 - 0.25 * windSpeed) * (tempF - 91.4) + 91.4; 
+    oldstyle = 0.081 * (3.71 * sqrtWind + 5.81 - 0.25 * windSpeed) * (tempF - 91.4) + 91.4; // old style formula
    cout << "The old style formula for wind chill produced " << endl;
     cout << oldstyle << endl;
     
     cout << " and the new style produced. " << endl;
     powWind = pow(windSpeed, 0.16);
-    newStyle = 35.74 + 0.6215 * tempF - 35.75 *  powWind + 0.4275 * tempF * powWind;
+    newStyle = 35.74 + 0.6215 * tempF - 35.75 *  powWind + 0.4275 * tempF * powWind; // new formula
     cout << newStyle << endl;
     
-    diffOfStyles = abs(oldstyle - newStyle);
+    diffOfStyles = abs(oldstyle - newStyle);  // takes the absoulte differnce from the two formulas
     
     cout << "The differnce between the styles is " << endl; 
     cout << diffOfStyles << endl;
     cout << " " << endl;
     
-    cout << "windSpeed:    Old formula:     New formula:      differnce:" << endl;
+    cout << "windSpeed:    Old formula:     New formula:      differnce:" << endl;  // table
     cout << "   " << windSpeed <<"          " << oldstyle << "           " << newStyle << "            " << diffOfStyles << endl;   
      
     
