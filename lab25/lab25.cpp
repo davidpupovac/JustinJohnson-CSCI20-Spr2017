f76e9fb423f0b0f395391468786d58edbffe47e4
Justin Johnson <jjohnson103@student.butte.edu>
2017-03-17 02:11:47 +0000

    Lab2.5 submission

--- lab25/lab25.cpp
+++ lab25/lab25.cpp
@@ -1,6 +1,6 @@
 // created by Justin Johnson
 // created on 2/27/17
-//  creating book information with class and objects
+//  this prgram will take info for 5 books and output  with use from class and objects
 
 #include <iostream>
 #include <string>
@@ -61,15 +61,162 @@ string Book::GetGenreOfBook() {
 }
 
 int main(){
-    Book bookinfo;
+    Book bookInfo;
     
-    string bookOne;  
-    string bookAuthor;
-    string bookCopyRight;
-    string bookGenre;
-    string bookAgeGroup;
+    string bookTitle1;  // book title, author etc..
+    string bookAuthor1;
+    int bookCopyRight1;
+    string bookGenre1;
+    int bookAgeGroup1;
     
-    cout << " Please enter the title of the book. " << endl;
-    getline(cin, );
+    cout << "Please enter the title for the first book. "<< endl;
+    getline(cin, bookTitle1);
+    bookInfo.SetBookTitle(bookTitle1);
+    
+    cout << " Author of the book? "<< endl;
+    getline(cin, bookAuthor1);
+    bookInfo.SetBookAuthor(bookAuthor1);
+    
+    cout << "Enter the year the book was copyrighted"<< endl;
+    cin >> bookCopyRight1;
+    bookInfo.SetCopyRightYear(bookCopyRight1);
+    
+    cout << "What is the gerne of the book? "<< endl;
+    getline(cin, bookGenre1);
+    bookInfo.SetGenreOfBook(bookGenre1);
+    
+    cout << " What is an apporite age to read this book? " << endl;
+    cin >> bookAgeGroup1;
+    bookInfo.SetAgeGroup(bookAgeGroup1); 
+    cout << endl;
 
+    cout << " We will repete this process four more times " << endl;
+    cout << endl;
+
+    string bookTitle2;  // book 2
+    string bookAuthor2;
+    int bookCopyRight2;
+    string bookGenre2;
+    int bookAgeGroup2;
+    
+    cout << "Please enter the title for the second book. " << endl;
+    getline(cin, bookTitle2);
+    bookInfo.SetBookTitle(bookTitle2);
+    
+    cout << " Author of the book? " << endl;
+    getline(cin, bookAuthor2);
+    bookInfo.SetBookAuthor(bookAuthor2);
+    
+    cout << "Enter the year the book was copyrighted" << endl;
+    cin >> bookCopyRight2;
+    bookInfo.SetCopyRightYear(bookCopyRight2);
+    
+    cout << "What is the gerne of the book? " << endl;
+    getline(cin, bookGenre2);
+    bookInfo.SetGenreOfBook(bookGenre2);
+    
+    cout << " What is an apporite age to read this book? " << endl;
+    cin >> bookAgeGroup2;
+    bookInfo.SetAgeGroup(bookAgeGroup2);
+    
+    string bookTitle3;  // book 3
+    string bookAuthor3;
+    int bookCopyRight3;
+    string bookGenre3;
+    int bookAgeGroup3;
+    
+    cout << "Please enter the title for the thrid book. "<< endl;
+    getline(cin, bookTitle3);
+    bookInfo.SetBookTitle(bookTitle3);
+    
+    cout << " Author of the book? " << endl;
+    getline(cin, bookAuthor3);
+    bookInfo.SetBookAuthor(bookAuthor3);
+    
+    cout << "Enter the year the book was copyrighted" << endl;
+    cin >> bookCopyRight3;
+    bookInfo.SetCopyRightYear(bookCopyRight3);
+    
+    cout << "What is the gerne of the book? " << endl;
+    getline(cin, bookGenre3);
+    bookInfo.SetGenreOfBook(bookGenre3);
+    
+    cout << " What is an apporite age to read this book? " << endl;
+    cin >> bookAgeGroup3;
+    bookInfo.SetAgeGroup(bookAgeGroup3);
+    
+    string bookTitle4;  // book 4
+    string bookAuthor4;
+    int bookCopyRight4;
+    string bookGenre4;
+    int bookAgeGroup4;
+    
+    cout << "Please enter the title for the fourth book. "<< endl;
+    getline(cin, bookTitle4);
+    bookInfo.SetBookTitle(bookTitle4);
+    
+    cout << " Author of the book? " << endl;
+    getline(cin, bookAuthor4);
+    bookInfo.SetBookAuthor(bookAuthor4);
+    
+    cout << "Enter the year the book was copyrighted" << endl;
+    cin >> bookCopyRight4;
+    bookInfo.SetCopyRightYear(bookCopyRight4);
+    
+    cout << "What is the gerne of the book? " << endl;
+    getline(cin, bookGenre4);
+    bookInfo.SetGenreOfBook(bookGenre4);
+    
+    cout << " What is an apporite age to read this book? " << endl;
+    cin >> bookAgeGroup4;
+    bookInfo.SetAgeGroup(bookAgeGroup4);
+    
+    string bookTitle5;  // book 5
+    string bookAuthor5;
+    int bookCopyRight5;
+    string bookGenre5;
+    int bookAgeGroup5;
+    
+    cout << "Please enter the title for the fith book. "<< endl;
+    getline(cin, bookTitle5);
+    bookInfo.SetBookTitle(bookTitle5);
+    
+    cout << " Author of the book? " << endl;
+    getline(cin, bookAuthor5);
+    bookInfo.SetBookAuthor(bookAuthor5);
+    
+    cout << "Enter the year the book was copyrighted" << endl;
+    cin >> bookCopyRight5;
+    bookInfo.SetCopyRightYear(bookCopyRight5);
+    
+    cout << "What is the gerne of the book? " << endl;
+    getline(cin, bookGenre5);
+    bookInfo.SetGenreOfBook(bookGenre5);
+    
+    cout << " What is an apporite age to read this book? " << endl;
+    cin >> bookAgeGroup5;
+    bookInfo.SetAgeGroup(bookAgeGroup5);
+    
+    cout << " Thank you here is your information you enterd for the books. " << endl;
+    cout << endl;
+    
+    cout << "Book 1:" << endl;
+    cout << bookTitle1 <<" "<< bookAuthor1 <<" "<< bookCopyRight1 <<" "<<  bookGenre1 <<" "<< bookAgeGroup1;
+    cout << endl;
+  
+    cout << "Book 2:" << endl;
+    cout << bookTitle2 <<" "<< bookAuthor2 <<" "<< bookCopyRight2 <<" "<<  bookGenre2 <<" "<< bookAgeGroup2;
+    cout << endl;  
+    
+        cout << "Book 3:" << endl;
+    cout << bookTitle3 <<" "<< bookAuthor3 <<" "<< bookCopyRight3 <<" "<<  bookGenre3 <<" "<< bookAgeGroup3;
+    cout << endl;
+    
+        cout << "Book 4:" << endl;
+    cout << bookTitle4 <<" "<< bookAuthor4 <<" "<< bookCopyRight4 <<" "<<  bookGenre4 <<" "<< bookAgeGroup4;
+    cout << endl;
+    
+        cout << "Book 5:" << endl;
+    cout << bookTitle5 <<" "<< bookAuthor5 <<" "<< bookCopyRight5 <<" "<<  bookGenre5 <<" "<< bookAgeGroup5;
+    cout << endl;
 }
