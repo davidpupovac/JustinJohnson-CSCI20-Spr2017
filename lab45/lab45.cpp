35fb5776db81391c80a2aecd27a6bf82cec2ca2b
Justin Johnson <jjohnson103@student.butte.edu>
2017-04-26 04:54:35 +0000

    Lab4.5 Submission

--- lab45/lab45.cpp
+++ lab45/lab45.cpp
@@ -8,51 +8,51 @@ using namespace std;
 
 int main(){
     
-    
     const int size1 = 11;
     const int size2 = 21;
     string firstName ; // first name
     string lastName ;  // lastname
     int username; // will allow choice for username
-
-     cout << "Enter your first name (10 or less characters)" << endl;
-       getline(cin, firstName);  // input for first name
-
-     cout << "Enter you last name now (20 or less characters)" << endl;
-       getline(cin, lastName);
+    
+    // input for first name
+    cout << "Enter your first name (10 or less characters)" << endl;
+    getline(cin, firstName); 
+    
+    // input for last name
+    cout << "Enter you last name now (20 or less characters)" << endl;
+    getline(cin, lastName);
        
-     if((firstName.length() > size1 || lastName.length() > size2)){
-        do {             
-            cout << "one of the names is more than allowed" << endl;
-            cout << "enter first name again." << endl;
-            getline(cin, firstName);  // input for first name
-            cout << "enter last name again." << endl;
-            getline(cin, lastName); // input for last name
+// names are longer than allowed then will make them re-enter names
+if((firstName.length() > size1 || lastName.length() > size2)){
+    do {             
+        cout << "one of the names is more than allowed" << endl;
+        cout << "enter first name again." << endl;
+        getline(cin, firstName);  // input for first name
+        cout << "enter last name again." << endl;
+        getline(cin, lastName); // input for last name
             
-        } while(firstName.length() > size1 || lastName.length() > size2);
-     } // closes if statement 
-///////////////////////////////////////////////////////////////////////////////////
+    } while(firstName.length() > size1 || lastName.length() > size2);
+} // closes if statement 
+/////////////////////////////////////////////////////////////////////////////////// 
      
-     // if first and last name are same will make them re-enter names
+// if first and last name are same will make them re-enter names
 if(firstName == lastName){
     do{
         cout << "Wrong.. Names enterd in are the same" << endl ;
         cout << "Enter your first name again" << endl;
-         getline(cin, firstName);  // input for first name
+        getline(cin, firstName);  // input for first name
         cout << "Enter your last name  again" << endl;
         getline(cin, lastName);  // input for first name
      }while(firstName == lastName); 
  } // closes if statement
- 
- cout << "Your name is " << firstName << " " << lastName << endl;  // outputs full name
+    
+    // outputs full name
+    cout << "Your name is " << firstName << " " << lastName << endl;
     
     // choices for usernames
-     cout << endl << "Which usernames would you like? Choose the number by name:" << endl << "1: ";
- cout << "Your name is " << firstName << " " << lastName << endl;  // outputs full name
+    cout << endl << "Which usernames would you like? Choose the number by name:" << endl;
     
-    // choices for usernames
-     cout << endl << "Which usernames would you like? Choose the number by name:" << endl;
-    
+    // usernames 
     cout << "1) " << firstName.at(0) << firstName.at(1) << lastName << endl;
     cout << "2) " << firstName << lastName << endl;
     cout << "3) " << firstName.at(0) << lastName << endl;
@@ -63,13 +63,13 @@ if(firstName == lastName){
     cin >> username;
     cout << endl;
     
-    // let them to chose username
+    // outputs username 
 switch(username){
-    // reuse loops to output username chosen
+   
     case 1: 
         cout << "your user name is: " ;
         cout << firstName.at(0) << firstName.at(1) << lastName << endl;
-        break;
+    break;
         
     case 2:
         cout << "your user name is: " ;
@@ -81,8 +81,8 @@ switch(username){
         cout << firstName.at(0) << lastName << endl;
     break;
          
-    // if wrong number chosen the make them restart    
+       
     default:
         cout << "Restart and choose correctly.";
-}
+} // closes switch
 }
