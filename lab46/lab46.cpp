1a863b8316f46c6b3aa5d646080cc5567964bbbf
Justin Johnson <jjohnson103@student.butte.edu>
2017-05-01 08:44:47 +0000

    lab4.6 submission

--- lab46/lab46.cpp
+++ lab46/lab46.cpp
@@ -6,22 +6,68 @@ using namespace std;
 
 
 int main(){
+    int number = 4;
+    int semester = 3;
     
-    int units = 12;
-    int A  = 4;
-    int b = 3;
-    string FileName;
-    int currentGPA ;
+    string name;
+    int units [number];
     
-    cout << "enter file name: (suzycue)" << endl;
-    cin >>  FileName;
+    double grades[number];
+    char gradeLetter[4];
+    // calculate semester gp
+    double totalPoints;
+    double totalUnits;
+    // semster gpa
+    int semesterGpa;
     
-    ofstream GPAFile("FileName.txt");
+    // 
+    ifstream suzycue;
+    ofstream gpaFile;
     
-    GPAFile.open("FileName");
-    if(GPAFile.is_open()){
-        cout << "file is open" << endl;
+    // open files
+     suzycue.open("input.text");
+     gpaFile.open("output.html");
+    
+    // makes sure files are open
+    if(gpaFile.is_open() && suzycue.is_open()){
+        cout << "files are open" << endl;
+} // closes if statement
+
+    // get name
+    getline(suzycue,name);
+    
+    // get semester
+    suzycue >> semester;
+    
+
+    for (int i = 0; i < number; i++) {
+
+        totalPoints = (units[i] * grades[i]);
+        totalUnits = units[i]; 
 }
+
+    semesterGpa = totalPoints / totalUnits;
+
+    
+    // outputs students info
+    gpaFile << "<!DOCTYPE html>" << endl;
+    gpaFile << "<html>" << endl;
+    gpaFile << "<head>" << endl;
+    gpaFile << "<title>gpa info</title>" << endl;
+    gpaFile << "</head>" << endl;
+    gpaFile << "<body>" << endl;
+    gpaFile << "<h1>gpa info</h1>" << endl;
+    gpaFile << "<p>" << endl;
+    gpaFile << "Name: " << name << endl;
+    gpaFile << "Semester: " << semester << endl;
+    gpaFile << "Semester GPA: " << semesterGpa << endl;
+    gpaFile << "</p>" << endl;
+    gpaFile << "</body>" << endl;
+    gpaFile << "</html>" << endl;   
+    
+    // closes files
+    suzycue.close();
+    gpaFile.close();
     
     
     return 0;
